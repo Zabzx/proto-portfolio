1d0282622c5571033d34ec781167de5f541e3791
Zabzx <88985587+Zabzx@users.noreply.github.com>
2021-11-29 12:00:54 -0400

    Menu changes
    Disabled scrolling when showing the menu to allow for a better UX experience.

--- proto port/main.js
+++ proto port/main.js
@@ -1,4 +1,6 @@
 // UI Elements
+const body = document.querySelector('body');
+const html = document.querySelector('html');
 const nav = document.querySelector('.navbar-container');
 const burger = document.querySelector('.burger');
 const burgerLines = document.querySelectorAll('.line');
@@ -9,12 +11,16 @@ burger.addEventListener('click', () => {
     nav.classList.toggle('show-nav')
 
     if (nav.classList.contains('show-nav')) {
+        body.style.overflow = 'hidden';
+        html.style.overflow = 'hidden';
         burgerLines.forEach(line => {
             line.style.backgroundColor = '#6E5AE9'
         })
     } else {
         burgerLines.forEach(line => {
             line.style.backgroundColor = 'white'
+            body.style.overflow = 'scroll';
+            html.style.overflow = 'scroll';
         })
     }
 })
